aad45da872965ed1230b9e9acb5b68b70fa84149
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-03-30 13:39:22 -0600

    se agrego el scafol

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app_flutter/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(child: Text('Hola mundo')),
+      home: HomeScreen(),
     );
   }
 }
